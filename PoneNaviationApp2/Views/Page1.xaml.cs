df33dc23562fa5f6e90a2732b1c09499e7132766
sami9290 <sami9290@gmail.com>
2016-07-25 22:17:02 +0200

    First Commit

---
+++ PoneNaviationApp2/Views/Page1.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System.Diagnostics;
+using Windows.UI.Xaml.Controls;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using System;
+using Facebook;
+using System.Text.RegularExpressions;
+
+namespace PoneNaviationApp2.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page1 : Page
+    {
+        public Page1()
+        {
+            this.InitializeComponent();
+          
+        }
+        public string realid;
+        public string sClientID ;
+        public string realData ;
+        public string AccessToken;
+        public DateTime TokenExpiry;
+        public string syncCurrentData="";
+        public class UserData { public string userEmail; public string userPassword; }
+        public void extractDATA(string clientData)
+        {
+            var posPLUS = clientData.IndexOf('+');
+            var posPLUS1 = clientData.IndexOf('#');
+            if (!(posPLUS < 0))
+            {
+                sClientID = clientData.Substring(0, posPLUS);
+                syncCurrentData = "";
+                realData = clientData.Substring(posPLUS1 + 1);
+            }
+
+        }
+        private async Task Login()
+        {
+            //Client ID of the Facebook App (retrieved from the Facebook Developers portal)
+            var clientId = "1779097902326012";
+            //Required permissions
+            var scope = "public_profile, email";
+
+            var redirectUri = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().ToString();
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = clientId,
+                redirect_uri = redirectUri,
+                response_type = "token",
+                scope = scope
+            });
+
+            Uri startUri = loginUrl;
+            Uri endUri = new Uri(redirectUri, UriKind.Absolute);
+
+
+#if WINDOWS_PHONE_APP
+    WebAuthenticationBroker.AuthenticateAndContinue(startUri, endUri, null, WebAuthenticationOptions.None);
+#endif
+
+        WebAuthenticationResult result = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, startUri, endUri);
+        await ParseAuthenticationResult(result);
+
+        }
+        public async Task ParseAuthenticationResult(WebAuthenticationResult result)
+        {
+            switch (result.ResponseStatus)
+            {
+                case WebAuthenticationStatus.ErrorHttp:
+                    Debug.WriteLine("Error");
+                    break;
+                case WebAuthenticationStatus.Success:
+                    var pattern = string.Format("{0}#access_token={1}&expires_in={2}", WebAuthenticationBroker.GetCurrentApplicationCallbackUri(), "(?<access_token>.+)", "(?<expires_in>.+)");
+                    var match = Regex.Match(result.ResponseData, pattern);
+
+                    var access_token = match.Groups["access_token"];
+                    var expires_in = match.Groups["expires_in"];
+
+                    AccessToken = access_token.Value;
+                    TokenExpiry = DateTime.Now.AddSeconds(double.Parse(expires_in.Value));
+
+                    break;
+                case WebAuthenticationStatus.UserCancel:
+                    Debug.WriteLine("Operation aborted");
+                    break;
+                default:
+                    break;
+            }
+        }
+        public static  Quobject.SocketIoClientDotNet.Client.Socket soketname;
+        public static Quobject.SocketIoClientDotNet.Client.Socket getsocket() { return soketname; }
+        public static string userid;
+        public static string getid() { return userid; }
+        private void button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var socket = Quobject.SocketIoClientDotNet.Client.IO.Socket("http://localhost:3000");
+            soketname = socket;
+            socket.On(Quobject.SocketIoClientDotNet.Client.Socket.EVENT_CONNECT, (data) =>
+            {
+                //socket.Emit("hi");
+                var userData = new UserData() { userEmail = "LTXuser@mail.com", userPassword = "hs-fulda" };
+
+                UserData[] arr = new UserData[] { userData };
+                string json_data = JsonConvert.SerializeObject(arr);
+                //var obstring = @"{ ""userEmail"": ""LTXuser@mail.com"", ""userPassword"": ""hs-fulda"" }";
+                //string json = "{\"userEmail\": \"LTXuser@mail.com\", \"userPassword\": \"hs-fulda\"}";
+
+                socket.Emit("client_login", json_data);
+                
+            });
+            socket.On("login_granted", (data) =>
+            {
+                extractDATA(data.ToString());
+             
+            });
+          
+            //await Task.Delay(2000);
+           
+                gotonextpage();
+            Debug.WriteLine("fff");
+        }
+        //async Task PutTaskDelay()
+        //{
+        //    //await Task.Delay(50000);
+        //}
+        public void gotonextpage()
+        {
+            
+            if (sClientID != null)
+            {
+                this.Frame.Navigate(typeof(Page2));
+        }
+    }
+    }
+}
---
+++ PoneNaviationApp2/Views/Page2.xaml.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.IO;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using Windows.UI.Core;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PoneNaviationApp2.Views
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class Page2 : Page
+    {
+        public string clientID;
+        public Page2()
+        {
+            this.InitializeComponent();
+            clientID = Page1.getid();
+        }
+        public static Quobject.SocketIoClientDotNet.Client.Socket soketname=Page1.getsocket();
+        public string realData;
+        public string curDoc = "";
+        public string syncCurrentData = "";
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+        private void editor_TextChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+        }
+        class Res
+        {
+            public String Name { get; set; }
+            public String ID { get; set; }
+        }
+        public string[] terms = new string[9];
+        public Windows.UI.Xaml.Input.Pointer ptr;
+        private async void button_Copy_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            int j = 0;
+            List<string> docs = new List<string>();
+            
+            if (clientID != null)
+            {
+                var sSTR = clientID + '+'+ curDoc+ '#' + "get files from the folder";
+                soketname.Emit("getLXdocs", sSTR);  // get existing LaTeX docs on the server 
+                Debug.WriteLine("fffffffffffffffffffff");
+                soketname.On("avail_docs", (data) =>
+                {
+                    extractDATA(data.ToString());
+                    //int start = data.ToString().IndexOf("+");
+                    //int stop = data.ToString().IndexOf("#");
+                    //string output = data.ToString().Substring(start + 1, stop - start - 1);
+                    //realData = output;
+                    ////Debug.WriteLine(realData);
+                    //terms[j] = realData;
+                    //j++;
+                    Debug.WriteLine(realData);
+                    int offset = realData.IndexOf("#");
+                    string docnames = realData.Substring(offset+1, realData.Length);
+                    int firstchar = 0;
+                    int lastchar = 0;
+                    for (int i = 1; i < 9; i++)
+                    {
+
+                        if (offset == -1) continue;
+                        offset = docnames.IndexOf("#", offset + 1);
+                        if (firstchar == 0 && lastchar == 0)
+                        {
+                            firstchar = offset;
+                        }
+                        else if (firstchar != 0 && lastchar == 0)
+                        {
+                            lastchar = offset;
+                            string output = docnames.Substring(firstchar + 1, lastchar - firstchar - 1);
+                            realData = output;
+                            firstchar = lastchar;
+                            lastchar = 0;
+                            //Debug.WriteLine(realData);
+                            terms[j] = realData;
+                            j++;
+                        }
+                    }
+                });
+                Debug.WriteLine(realData);
+            }
+            await Task.Delay(1000);
+            gotonextpage(j);
+        }
+
+        public async void gotonextpage(int j)
+        {
+            await Task.Delay(1000);
+            int k = 0;
+            List<Res> myList = new List<Res>();
+            for (int u = 0; u <= j; u++)
+            {
+                Res binder1 = new Res();
+                binder1.Name = terms[u];
+                binder1.ID = terms[u];
+                myList.Add(binder1);
+                Debug.WriteLine(terms[u]);
+            }
+            lstData.ItemsSource = myList;
+        }
+
+        private async void Open_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = sender as Button;
+
+            Debug.WriteLine(clickedButton.DataContext.ToString());
+            curDoc = clickedButton.DataContext.ToString();
+            var sSTR = clientID + '+' + clickedButton.DataContext.ToString();
+            Debug.WriteLine(clientID + '+' + clickedButton.DataContext.ToString());
+            soketname.Emit("get_file", clientID + '+'+ curDoc+ '#' + clickedButton.DataContext.ToString());
+            soketname.On("deliver_doc", (data) =>
+            {
+                extractDATA(data.ToString());
+            });
+            await Task.Delay(1000);
+            editor.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+            button_Copy.Margin = new Thickness(24, 490, 0, 0);
+
+            // Load the file into the Document property of the RichEditBox.
+
+            soketname.On("editing_granted", (data) =>
+           {
+               //    Debug.WriteLine("zzzzzzzzzzzzzzzzzzzzzz");
+                  extractDATA(data.ToString());
+               //    editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, realData);
+               //    //await Task.Delay(1000);
+               //    //Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+               //    //  () =>
+               //    //  {
+
+               //    //      update();
+               //    //  }
+               //    //  );
+           });
+            soketname.On("server_character", (data) =>
+            {
+                extractDATA(data.ToString());
+                Debug.WriteLine(realData);
+                //await Task.Delay(1000);
+                Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    () =>
+                    {
+
+                        update();
+                    }
+                    );
+            });
+            editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, realData);
+            Debug.WriteLine(realData);
+            var sTxt = "User: " + realData + " wants to edit the file: " + clickedButton.DataContext.ToString() + "!\n\n";
+            //soketname.Emit("edit_allowed", sTxt);
+        }
+
+        private  void button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            editor.Visibility = Windows.UI.Xaml.Visibility.Visible;
+          
+
+            string text = File.ReadAllText("emty.txt");
+        
+            // Load the file into the Document property of the RichEditBox.
+            editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, text);
+
+            button_Copy.Margin = new Thickness(24, 490, 0, 0);
+            soketname.On("deliver_doc", (data) =>
+            {
+                extractDATA(data.ToString());
+            });
+            //soketname.On("editing_granted", (data) =>
+            //{
+            //    Debug.WriteLine("ggggggggggggggggggggggg");
+            //    extractDATA(data.ToString());
+
+            //    //await Task.Delay(1000);
+            //    Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            //      () =>
+            //      {
+
+            //          update();
+            //      }
+            //      );
+            //});
+            soketname.On("server_character",  (data) =>
+           {
+               extractDATA(data.ToString());
+               Debug.WriteLine(realData);
+               //await Task.Delay(1000);
+               Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                   () =>
+                   {
+
+                       update();
+                   }
+                   );
+           }); 
+        }
+        public async void update()
+        {
+            await Task.Delay(1000);
+            
+            if (syncCurrentData.Substring(0, 3) == curDoc.Substring(0, 3)|| syncCurrentData==curDoc)
+            {
+                //editor.Document.GetText(Windows.UI.Text.TextGetOptions.None, out text);
+                //int start = 68;
+                //int end = text.Length - 15;
+                //editor.Document.Selection.SetRange(start, end);
+                //editor.Document.SetText(TextSetOptions.None, string.Empty);
+                editor.Document.SetText(TextSetOptions.ApplyRtfDocumentDefaults, realData);
+            }
+        }
+        public  void extractDATA(string clientData)
+        {
+            int start = clientData.IndexOf("+") + 1;
+            int end = clientData.IndexOf("#", start);
+            var posPLUS = clientData.IndexOf('+');
+            var posPLUS1 = clientData.IndexOf('#');
+            if (!(posPLUS < 0))
+            {
+                clientID = clientData.Substring(0, posPLUS);
+                syncCurrentData = clientData.Substring(start, posPLUS1 - posPLUS);
+                realData = clientData.Substring(posPLUS1 - posPLUS + 2);
+                //syncCurrentData= Regex.Match(clientData, @"\(+\w+)\+").Groups[1].Value;
+                //realData= clientData.Substring(posPLUS1+1);
+            }
+        }
+        //public  void extractDATAall(string clientData)
+        //{
+        //    //await Task.Delay(1000);
+        //    var posPLUS = clientData.IndexOf('+');
+        //    if (!(posPLUS < 0))
+        //    {
+        //        realData = clientData.Substring(posPLUS + 1);
+        //    }
+        //}
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string content;
+            var socketpage = Page1.getsocket();
+            var sCURRENT_LTXdoc = clientID + "@document";
+            editor.Document.GetText(Windows.UI.Text.TextGetOptions.None, out content);
+            var sSTR2 = clientID + '+' + sCURRENT_LTXdoc + '+' + content;
+            socketpage.Emit("save_doc", sSTR2);
+        }
+
+        private void editor_KeyUp(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            string text;
+            editor.Document.GetText(Windows.UI.Text.TextGetOptions.None, out text);          
+            string sSTR2 = clientID +'+' + curDoc + '#' +  text;
+            soketname.Emit("client_character", sSTR2);
+            Debug.WriteLine(sSTR2);
+        }
+
+
+    }
+}
