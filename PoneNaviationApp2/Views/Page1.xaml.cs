c6c338668878525406a7bb611c39502f835893a0
sami9290 <sami9290@gmail.com>
2016-07-26 04:44:10 +0200

    Adding some comments and fixing bugs!

--- PoneNaviationApp2/Views/Page1.xaml.cs
+++ PoneNaviationApp2/Views/Page1.xaml.cs
@@ -10,34 +10,37 @@ using System.Text.RegularExpressions;
 namespace PoneNaviationApp2.Views
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// This is the main login page were the login id is generated and passed to the 
     /// </summary>
     public sealed partial class Page1 : Page
     {
         public Page1()
         {
             this.InitializeComponent();
-          
         }
+
+        ///Some public variables
         public string realid;
         public string sClientID ;
         public string realData ;
         public string AccessToken;
         public DateTime TokenExpiry;
         public string syncCurrentData="";
+        public static Quobject.SocketIoClientDotNet.Client.Socket soketname;
+        public static Quobject.SocketIoClientDotNet.Client.Socket getsocket() { return soketname; }
+        public static string userid;
+        public static string getid() { return userid; }
         public class UserData { public string userEmail; public string userPassword; }
+
+        /// Getting the clientID from the server
         public void extractDATA(string clientData)
         {
-            var posPLUS = clientData.IndexOf('+');
-            var posPLUS1 = clientData.IndexOf('#');
-            if (!(posPLUS < 0))
-            {
+            Debug.WriteLine(clientData);
+           var posPLUS = clientData.IndexOf('+');
                 sClientID = clientData.Substring(0, posPLUS);
-                syncCurrentData = "";
-                realData = clientData.Substring(posPLUS1 + 1);
-            }
-
         }
+
+        /// Facebook Login #Under Construction!!#
         private async Task Login()
         {
             //Client ID of the Facebook App (retrieved from the Facebook Developers portal)
@@ -54,18 +57,12 @@ namespace PoneNaviationApp2.Views
                 response_type = "token",
                 scope = scope
             });
-
             Uri startUri = loginUrl;
             Uri endUri = new Uri(redirectUri, UriKind.Absolute);
-
-
-#if WINDOWS_PHONE_APP
-    WebAuthenticationBroker.AuthenticateAndContinue(startUri, endUri, null, WebAuthenticationOptions.None);
-#endif
-
         WebAuthenticationResult result = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, startUri, endUri);
         await ParseAuthenticationResult(result);
 
+            /// Facebook Login #Under Construction!!#
         }
         public async Task ParseAuthenticationResult(WebAuthenticationResult result)
         {
@@ -92,49 +89,33 @@ namespace PoneNaviationApp2.Views
                     break;
             }
         }
-        public static  Quobject.SocketIoClientDotNet.Client.Socket soketname;
-        public static Quobject.SocketIoClientDotNet.Client.Socket getsocket() { return soketname; }
-        public static string userid;
-        public static string getid() { return userid; }
+        /// Buttonclick for the login 
         private void button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            var socket = Quobject.SocketIoClientDotNet.Client.IO.Socket("http://localhost:3000");
-            soketname = socket;
-            socket.On(Quobject.SocketIoClientDotNet.Client.Socket.EVENT_CONNECT, (data) =>
+            soketname = Quobject.SocketIoClientDotNet.Client.IO.Socket("http://localhost:3000");
+            soketname.On(Quobject.SocketIoClientDotNet.Client.Socket.EVENT_CONNECT, (data) =>
             {
-                //socket.Emit("hi");
                 var userData = new UserData() { userEmail = "LTXuser@mail.com", userPassword = "hs-fulda" };
-
                 UserData[] arr = new UserData[] { userData };
                 string json_data = JsonConvert.SerializeObject(arr);
-                //var obstring = @"{ ""userEmail"": ""LTXuser@mail.com"", ""userPassword"": ""hs-fulda"" }";
-                //string json = "{\"userEmail\": \"LTXuser@mail.com\", \"userPassword\": \"hs-fulda\"}";
-
-                socket.Emit("client_login", json_data);
-                
+                soketname.Emit("client_login", json_data);
             });
-            socket.On("login_granted", (data) =>
-            {
-                extractDATA(data.ToString());
-             
-            });
-          
-            //await Task.Delay(2000);
-           
-                gotonextpage();
+            gotonextpage();
             Debug.WriteLine("fff");
         }
-        //async Task PutTaskDelay()
-        //{
-        //    //await Task.Delay(50000);
-        //}
-        public void gotonextpage()
+        private async void gotonextpage()
         {
-            
+            //await Task.Delay(2000);
+            soketname.On("login_granted", (data2) =>
+            {
+                extractDATA(data2.ToString());
+            });
+            await Task.Delay(3000);
             if (sClientID != null)
             {
-                this.Frame.Navigate(typeof(Page2));
+            Debug.WriteLine(sClientID);
+            this.Frame.Navigate(typeof(Page2),sClientID);
+            }
         }
-    }
     }
 }
--- PoneNaviationApp2/Views/Page2.xaml.cs
+++ PoneNaviationApp2/Views/Page2.xaml.cs
@@ -12,92 +12,87 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using Windows.UI.Popups;
 
 namespace PoneNaviationApp2.Views
 {
-    /// <summary>
+    /// Auther @Sami
+    /// In this page the main sending and receiving to socket server operation exists
+    /// Three main option, open new LateX file, open an already existing LateX file from the server or save your file as new file
     /// 
-    /// </summary>
     public sealed partial class Page2 : Page
-    {
-        public string clientID;
+    {  
         public Page2()
         {
             this.InitializeComponent();
-            clientID = Page1.getid();
         }
+
+        /// Some variables used in this view 
         public static Quobject.SocketIoClientDotNet.Client.Socket soketname=Page1.getsocket();
         public string realData;
+        public static string clientID ;
         public string curDoc = "";
         public string syncCurrentData = "";
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        public string[] terms = new string[9];
+
+        /// The Navegation from the prevoius page with the clientid as a parameter
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
+            try
+            {
+                clientID = e.Parameter.ToString();
+            }
+            catch (Exception)
+            {
+                var dialog = new MessageDialog("Please Login First!!!");
+                await dialog.ShowAsync();
+            }
         }
-        private void editor_TextChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-        }
+
+        ///  a calass used in viwing and opening the documents
         class Res
         {
             public String Name { get; set; }
             public String ID { get; set; }
         }
-        public string[] terms = new string[9];
-        public Windows.UI.Xaml.Input.Pointer ptr;
+
+        /// Get the available documents on the server into a matrix
         private async void button_Copy_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             int j = 0;
             List<string> docs = new List<string>();
-            
             if (clientID != null)
             {
-                var sSTR = clientID + '+'+ curDoc+ '#' + "get files from the folder";
+                var sSTR = clientID + '+' + "get files from the folder";
                 soketname.Emit("getLXdocs", sSTR);  // get existing LaTeX docs on the server 
-                Debug.WriteLine("fffffffffffffffffffff");
                 soketname.On("avail_docs", (data) =>
                 {
-                    extractDATA(data.ToString());
-                    //int start = data.ToString().IndexOf("+");
-                    //int stop = data.ToString().IndexOf("#");
-                    //string output = data.ToString().Substring(start + 1, stop - start - 1);
-                    //realData = output;
-                    ////Debug.WriteLine(realData);
-                    //terms[j] = realData;
-                    //j++;
-                    Debug.WriteLine(realData);
+                    extractDATAAll(data.ToString());    
                     int offset = realData.IndexOf("#");
-                    string docnames = realData.Substring(offset+1, realData.Length);
+                    realData = "#" + realData;
                     int firstchar = 0;
                     int lastchar = 0;
-                    for (int i = 1; i < 9; i++)
+                    for (int i = 1; i < 8; i++)
                     {
-
-                        if (offset == -1) continue;
-                        offset = docnames.IndexOf("#", offset + 1);
-                        if (firstchar == 0 && lastchar == 0)
-                        {
-                            firstchar = offset;
-                        }
-                        else if (firstchar != 0 && lastchar == 0)
-                        {
+                            if (offset == -1) continue;
+                            offset = realData.IndexOf("#", offset + 1);         
                             lastchar = offset;
-                            string output = docnames.Substring(firstchar + 1, lastchar - firstchar - 1);
-                            realData = output;
+                            string output = realData.Substring(firstchar + 1, lastchar - firstchar - 1);
                             firstchar = lastchar;
                             lastchar = 0;
-                            //Debug.WriteLine(realData);
-                            terms[j] = realData;
+                            terms[j] = output;
                             j++;
-                        }
                     }
                 });
-                Debug.WriteLine(realData);
             }
-            await Task.Delay(1000);
+            await Task.Delay(3000);
             gotonextpage(j);
         }
 
+        /// View the previous matrix of the documents
         public async void gotonextpage(int j)
         {
+            j = 6;
             await Task.Delay(1000);
             int k = 0;
             List<Res> myList = new List<Res>();
@@ -107,50 +102,38 @@ namespace PoneNaviationApp2.Views
                 binder1.Name = terms[u];
                 binder1.ID = terms[u];
                 myList.Add(binder1);
-                Debug.WriteLine(terms[u]);
             }
             lstData.ItemsSource = myList;
         }
 
+        /// Open a selected Latex File
         private async void Open_Click(object sender, RoutedEventArgs e)
         {
             Button clickedButton = sender as Button;
-
-            Debug.WriteLine(clickedButton.DataContext.ToString());
             curDoc = clickedButton.DataContext.ToString();
             var sSTR = clientID + '+' + clickedButton.DataContext.ToString();
-            Debug.WriteLine(clientID + '+' + clickedButton.DataContext.ToString());
-            soketname.Emit("get_file", clientID + '+'+ curDoc+ '#' + clickedButton.DataContext.ToString());
+            soketname.Emit("get_file", clientID + '+'+ curDoc+ '#' + curDoc);
             soketname.On("deliver_doc", (data) =>
             {
                 extractDATA(data.ToString());
             });
             await Task.Delay(1000);
             editor.Visibility = Windows.UI.Xaml.Visibility.Visible;
-
             button_Copy.Margin = new Thickness(24, 490, 0, 0);
-
-            // Load the file into the Document property of the RichEditBox.
-
             soketname.On("editing_granted", (data) =>
            {
-               //    Debug.WriteLine("zzzzzzzzzzzzzzzzzzzzzz");
-                  extractDATA(data.ToString());
-               //    editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, realData);
-               //    //await Task.Delay(1000);
-               //    //Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-               //    //  () =>
-               //    //  {
+               extractDATA(data.ToString());
+               Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                 () =>
+                 {
 
-               //    //      update();
-               //    //  }
-               //    //  );
+                     update();
+                 }
+                 );
            });
             soketname.On("server_character", (data) =>
             {
                 extractDATA(data.ToString());
-                Debug.WriteLine(realData);
-                //await Task.Delay(1000);
                 Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
                     () =>
                     {
@@ -160,18 +143,14 @@ namespace PoneNaviationApp2.Views
                     );
             });
             editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, realData);
-            Debug.WriteLine(realData);
             var sTxt = "User: " + realData + " wants to edit the file: " + clickedButton.DataContext.ToString() + "!\n\n";
-            //soketname.Emit("edit_allowed", sTxt);
         }
 
-        private  void button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        ///Open a new Latex file
+        private void button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            editor.Visibility = Windows.UI.Xaml.Visibility.Visible;
-          
-
+            editor.Visibility = Windows.UI.Xaml.Visibility.Visible;         
             string text = File.ReadAllText("emty.txt");
-        
             // Load the file into the Document property of the RichEditBox.
             editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, text);
 
@@ -180,73 +159,59 @@ namespace PoneNaviationApp2.Views
             {
                 extractDATA(data.ToString());
             });
-            //soketname.On("editing_granted", (data) =>
-            //{
-            //    Debug.WriteLine("ggggggggggggggggggggggg");
-            //    extractDATA(data.ToString());
-
-            //    //await Task.Delay(1000);
-            //    Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            //      () =>
-            //      {
-
-            //          update();
-            //      }
-            //      );
-            //});
+            soketname.On("editing_granted",  (data) =>
+            {
+                extractDATA(data.ToString());
+            });
             soketname.On("server_character",  (data) =>
-           {
-               extractDATA(data.ToString());
-               Debug.WriteLine(realData);
-               //await Task.Delay(1000);
-               Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-                   () =>
-                   {
+          {
+              extractDATAAll(data.ToString());
+              Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                  () =>
+                  {
 
-                       update();
-                   }
-                   );
-           }); 
+                      update();
+                  }
+                  );
+          }); 
         }
+
+        /// Update the textblock with the new values
         public async void update()
         {
             await Task.Delay(1000);
-            
-            if (syncCurrentData.Substring(0, 3) == curDoc.Substring(0, 3)|| syncCurrentData==curDoc)
+            Debug.WriteLine(syncCurrentData + " >>>  " + curDoc);
+            if (syncCurrentData == curDoc || syncCurrentData.Substring(0, 3) == curDoc.Substring(0, 3))
             {
-                //editor.Document.GetText(Windows.UI.Text.TextGetOptions.None, out text);
-                //int start = 68;
-                //int end = text.Length - 15;
-                //editor.Document.Selection.SetRange(start, end);
-                //editor.Document.SetText(TextSetOptions.None, string.Empty);
                 editor.Document.SetText(TextSetOptions.ApplyRtfDocumentDefaults, realData);
             }
         }
-        public  void extractDATA(string clientData)
+
+        /// Get the content of the latex file and the document name
+        public void extractDATA(string clientData)
         {
             int start = clientData.IndexOf("+") + 1;
             int end = clientData.IndexOf("#", start);
             var posPLUS = clientData.IndexOf('+');
             var posPLUS1 = clientData.IndexOf('#');
-            if (!(posPLUS < 0))
+            if (clientData.Contains("+#"))
             {
-                clientID = clientData.Substring(0, posPLUS);
-                syncCurrentData = clientData.Substring(start, posPLUS1 - posPLUS);
-                realData = clientData.Substring(posPLUS1 - posPLUS + 2);
-                //syncCurrentData= Regex.Match(clientData, @"\(+\w+)\+").Groups[1].Value;
-                //realData= clientData.Substring(posPLUS1+1);
+                realData = clientData.Substring(posPLUS1  + 1);
             }
+            else
+                syncCurrentData = clientData.Substring(clientData.IndexOf("+") + 1, clientData.IndexOf("#"));
+            Debug.WriteLine(syncCurrentData);
+            realData = clientData.Substring(posPLUS1 + 1);
         }
-        //public  void extractDATAall(string clientData)
-        //{
-        //    //await Task.Delay(1000);
-        //    var posPLUS = clientData.IndexOf('+');
-        //    if (!(posPLUS < 0))
-        //    {
-        //        realData = clientData.Substring(posPLUS + 1);
-        //    }
-        //}
 
+        /// Get only the content of the documnet
+        public void extractDATAAll(string clientData)
+        {
+            var posPLUS = clientData.IndexOf('+');
+            realData = clientData.Substring(posPLUS+1);
+        }
+
+        /// Save the nocument on the server as a new LateX file
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             string content;
@@ -257,15 +222,13 @@ namespace PoneNaviationApp2.Views
             socketpage.Emit("save_doc", sSTR2);
         }
 
+        /// Key event to sent the updated vcontent of the textbloc to the server
         private void editor_KeyUp(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
         {
             string text;
             editor.Document.GetText(Windows.UI.Text.TextGetOptions.None, out text);          
-            string sSTR2 = clientID +'+' + curDoc + '#' +  text;
+            string sSTR2 = clientID +'+' + syncCurrentData + '#' +  text;
             soketname.Emit("client_character", sSTR2);
-            Debug.WriteLine(sSTR2);
         }
-
-
     }
 }
